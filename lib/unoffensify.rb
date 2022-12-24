43c1f5a1f072f9e8256c89b325ecdf7e6b714c73
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2022-12-23 20:53:47 -0700

    initial commit

---
+++ lib/unoffensify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "unoffensify/version"
+
+module Unoffensify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/unoffensify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Unoffensify
+  VERSION = "0.1.0"
+end
