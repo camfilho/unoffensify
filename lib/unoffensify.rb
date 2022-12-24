7a36f13379166b85640808047b89ad412606c6c0
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2022-12-23 22:01:00 -0700

    initial specs

--- lib/unoffensify.rb
+++ lib/unoffensify.rb
@@ -4,5 +4,21 @@ require_relative "unoffensify/version"
 
 module Unoffensify
   class Error < StandardError; end
-  # Your code goes here...
+  Terms = ['blacklist', 'black_list', 'white_list'].freeze
+
+
+  def self.check_files_to_change
+    files = []
+    Dir.glob("tmp/*.rb") do |file|
+      content = File.open(file, "r") { |f| f.read }
+
+      file_name = File.basename(file)
+      Terms.each do |term|
+        files << file if file_name.match(/#{term}/i)
+        files << file if content.match(/#{term}/i)
+      end
+    end
+
+    files
+  end
 end
--- test/test_unoffensify.rb
+++ test/test_unoffensify.rb
@@ -7,7 +7,25 @@ class TestUnoffensify < Minitest::Test
     refute_nil ::Unoffensify::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_check_files_to_change_by_content
+    # create enmpty file named black_list.rb
+    File.open("tmp/denylist.rb", "w") do |f|
+      f.write("class BlackList")
+      f.write("end")
+    end
+    files = Unoffensify.check_files_to_change
+    File.delete("tmp/denylist.rb")
+
+    assert_equal ["tmp/denylist.rb"], files
+  end
+
+  def test_check_files_to_change
+    # create enmpty file named black_list.rb
+    File.open("tmp/black_list.rb", "w") {}
+
+    assert_equal ["tmp/black_list.rb"], Unoffensify.check_files_to_change
+
+    #delete file
+    File.delete("tmp/black_list.rb")
   end
 end
